baaa218f4e73154add5ad78d650afa17c29e918f
JeremyN05 <narvaezlobatojeremy@gmail.com>
2025-02-05 11:20:46 +0100

    Ejercicios 3 y 4 acabados

---
+++ Ejercicio_5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int n;
+	int m;
+
+	printf("Introduzca el números de filas: ");
+	scanf("%d", &n);
+
+	prinft("Introduzca el número de columnas: ");
+	scanf("%d", &m);
+
+	int ** arr = (int **) malloc (n * sizeof(int))
+
+}
